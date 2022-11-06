c9cb0f3a8fc6be8387d8e4127c85f14971eed1c0
blacktoast <dbstjd100@naver.com>
2022-11-07 00:26:23 +0900

    feat: nft 컨트랙트

---
+++ simpleNft/contract/contants.ts
@@ -0,0 +1,4 @@
+export const WHITELIST_CONTRACT_ADDRESS =
+  '0xf7c3694D1e3d1F2aC1d501D3498b4Db79DfA638A';
+// URL to extract Metadata for a Crypto Dev NFT
+export const METADATA_URL = 'https://nft-collection-sneh1999.vercel.app/api/';
